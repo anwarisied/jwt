f0d666f66da10dcfd841c50d4a0cad4b712833a5
Anwar Isied <anwar.isied@outlook.com>
2023-01-14 18:12:13 +0200

    Initial spring boot

---
+++ src/main/java/com/rm/roadmaps/RoadmapsApplication.java
@@ -0,0 +1,13 @@
+package com.rm.roadmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rm/roadmaps/RoadmapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.roadmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
